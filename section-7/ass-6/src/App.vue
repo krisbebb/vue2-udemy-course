1ff280a6dc97818f631caa8ea1ab6b37913d888a
krisbebb <krisbebb@gmail.com>
2020-08-07 12:23:38 +1000

    completed ass-6

---
+++ section-7/ass-6/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <app-server-details></app-server-details>
+      <app-servers></app-servers>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import Servers from "./Servers.vue";
+import ServerDetails from "./ServerDetails.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-servers": Servers,
+    "app-server-details": ServerDetails,
+  },
+};
+</script>
+
+<style></style>
